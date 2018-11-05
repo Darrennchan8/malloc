ae435fefdc17f41e0f449d7895cee2d5b89388dd
Darren Chan <darrennchan8@gmail.com>
2018-11-05 01:25:23 -0500

    Fix imports and header file.

--- malloc.h
+++ malloc.h
@@ -5,9 +5,13 @@
 
 #ifndef ASSIGN3_ASSIGN3_H
 #define ASSIGN3_ASSIGN3_H
-//
-//void* malloc(size_t size);
-//void* realloc(void* ptr, size_t size);
-//void* calloc(size_t num_elements, size_t element_size);
+
+void* malloc(size_t size);
+
+void* realloc(void* ptr, size_t size);
+
+void* calloc(size_t num_elements, size_t element_size);
+
+void free(void* ptr);
 
 #endif //ASSIGN3_ASSIGN3_H
